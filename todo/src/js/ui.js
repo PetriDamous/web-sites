f279ea37027728ccca5093dfcdf3cf3a370fc91e
dwilliams64 <dwilliams_64@yahoo.com>
2021-08-22 09:31:49 -0500

    deleted to-do, fixed navigation links for jake and elwood, updated index.html with more project links

--- todo/src/js/ui.js
+++ todo/src/js/ui.js
@@ -1,27 +1,29 @@
-import Storage from './storage';
-import {getColorCoords} from './utilis/utilis';
-class UI {  
-    constructor() {
-        this.$notesArea = document.querySelector('#notes-area');
-        this.$msg = document.querySelector('#message');
-        this.$formTitle = document.querySelector('#form-main #title');
-        this.$formBtns = document.querySelector('#form-buttons-main');
-        this.$colorToolTip = document.querySelector('.color-tooltip');
+import Storage from "./storage";
+import { getColorCoords } from "./utilis/utilis";
+class UI {
+  constructor() {
+    this.$notesArea = document.querySelector("#notes-area");
+    this.$msg = document.querySelector("#message");
+    this.$formTitle = document.querySelector("#form-main #title");
+    this.$formBtns = document.querySelector("#form-buttons-main");
+    this.$colorToolTip = document.querySelector(".color-tooltip");
 
-        this.$modal = document.querySelector('.modal');
-        this.$modelTitle = document.querySelector('.modal #title');
-        this.$modelTitle = document.querySelector('.modal #body');
-    }   
+    this.$modal = document.querySelector(".modal");
+    this.$modelTitle = document.querySelector(".modal #title");
+    this.$modelTitle = document.querySelector(".modal #body");
+  }
 
-    renderNotes() {
-        this.closeColor();
-        
-        const notes = Storage.getNotes();
+  renderNotes() {
+    this.closeColor();
 
-        console.log(notes)
+    const notes = Storage.getNotes();
 
-        this.$notesArea.innerHTML = notes.map((note) => 
-            `<div class="card" data-color="${note.color}" data-id="${note.id}">
+    console.log(notes);
+
+    this.$notesArea.innerHTML = notes
+      .map(
+        (note) =>
+          `<div class="card" data-color="${note.color}" data-id="${note.id}">
                 <h3 class="card__title">${note.title}</h3>
                 <div class="card__body">${note.body}</div>
                 <div class="card__options">
@@ -31,85 +33,85 @@ class UI {
                     <button class="btn btn-delete">Delete</button>
                 </div>
             </div>`
-        ).join('');
+      )
+      .join("");
+  }
+
+  displayMsg(action, status) {
+    switch (action) {
+      case "add":
+        this.$msg.textContent = "Note added.";
+        break;
+      case "delete":
+        this.$msg.textContent = "Note deleted";
+        break;
+      case "update":
+        this.$msg.textContent = "Note updated";
+        break;
+      case "validation":
+        this.$msg.textContent = "Please write a note.";
+        break;
     }
 
-    displayMsg(action, status) {        
-
-        switch (action) {
-            case 'add':
-                this.$msg.textContent = 'Note added.';
-            break;
-            case 'delete':
-                this.$msg.textContent = 'Note deleted';
-            break;
-            case 'update':
-                this.$msg.textContent = 'Note updated';
-            break;
-            case 'validation':
-                this.$msg.textContent = 'Please write a note.';
-            break;
-        }
-
-        if (status === 'danger') {
-            this.$msg.classList.remove('theme-success');
-            this.$msg.classList.add('theme-danger');
-        } else {
-            this.$msg.classList.remove('theme-danger');
-            this.$msg.classList.add('theme-success');  
-        }
-
-        this.$msg.style.visibility = 'visible';
-
-        setTimeout(() => {
-            this.$msg.style.visibility = 'hidden';
-        }, 3000);
+    if (status === "danger") {
+      this.$msg.classList.remove("theme-success");
+      this.$msg.classList.add("theme-danger");
+    } else {
+      this.$msg.classList.remove("theme-danger");
+      this.$msg.classList.add("theme-success");
     }
 
-    clearForm(title, body) {
-        title.value = '';
-        body.value = '';     
-    }
+    this.$msg.style.visibility = "visible";
 
-    openForm() {
-        this.closeColor();
-        this.$formTitle.style.display = 'block';
-        this.$formBtns.style.display = 'block';  
-    }
+    setTimeout(() => {
+      this.$msg.style.visibility = "hidden";
+    }, 3000);
+  }
 
-    closeForm() {
-        this.$formTitle.style.display = 'none';
-        this.$formBtns.style.display = 'none';
-    }
+  clearForm(title, body) {
+    title.value = "";
+    body.value = "";
+  }
 
-    openColor(closetColorBtn) { 
-        this.closeColor();
-        getColorCoords(closetColorBtn);
+  openForm() {
+    this.closeColor();
+    this.$formTitle.style.display = "block";
+    this.$formBtns.style.display = "block";
+  }
 
-        this.$colorToolTip.style.display = 'flex';
-    }   
+  closeForm() {
+    this.$formTitle.style.display = "none";
+    this.$formBtns.style.display = "none";
+  }
 
-    closeColor() {
-        this.$colorToolTip.style.display = 'none'; 
-    }
+  openColor(closetColorBtn) {
+    this.closeColor();
+    getColorCoords(closetColorBtn);
 
-    openModal({dataset: {id}}) { 
-        this.closeColor();       
-        const notes = Storage.getNotes();
-        const note = notes.find(note => note.id === id);
+    this.$colorToolTip.style.display = "flex";
+  }
 
-        const {title, body} = note;
+  closeColor() {
+    this.$colorToolTip.style.display = "none";
+  }
 
-        this.$modal.dataset.id = id;
-        this.$modelTitle.value = title;
-        this.$modelTitle.value = body;        
+  openModal({ dataset: { id } }) {
+    this.closeColor();
+    const notes = Storage.getNotes();
+    const note = notes.find((note) => note.id === id);
 
-        this.$modal.classList.add('open-modal');
-    }
+    const { title, body } = note;
 
-    closeModal() {
-        this.$modal.classList.remove('open-modal');
-    } 
+    this.$modal.dataset.id = id;
+    this.$modelTitle.value = title;
+    this.$modelTitle.value = body;
+
+    this.$modal.classList.add("open-modal");
+  }
+
+  closeModal() {
+    this.$modal.classList.remove("open-modal");
+  }
 }
 
 export default UI;
